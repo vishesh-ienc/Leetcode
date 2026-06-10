e4ae914029b9947ebda0a94e3fd2541e57ce776f
vishesh jiwnani <vishesh.ienc@gmail.com>
2026-06-10 12:46:46 +0530

    intial commit

---
+++ arrays/LC_121.cpp
@@ -0,0 +1,10 @@
+// Q.Best Time to Buy and Sell Stock
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+#include<bits/stdc++.h> 
+
+vector<int> = {7,1,5,3,6,4};
